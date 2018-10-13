7c60c3130c0f6e0d982a7887a16f3b351c465e1a
Ddone <mynameisthenekit@gmail.com>
2018-10-13 11:28:21 +0300

    Cache

---
+++ sub_generator/api.py
@@ -0,0 +1,72 @@
+import speech_recognition as sr
+import subprocess
+import os
+import hashlib
+import json
+
+
+
+if "cache.json" not in os.listdir():
+    with open("cache.json","w") as i:
+        i.write("{}")
+
+
+
+def write_cache(key, value):
+    data = 0
+    with open("cache.json","r") as i:
+        buf = i.read()
+        data = json.loads(buf)
+    data[key] = value
+    with open("cache.json","w") as o:
+        o.write(json.dumps(data))
+
+
+
+def get_cache(key):
+    buf = 0
+    with open("cache.json","r") as i:
+        buf = i.read()
+    data = json.loads(buf)
+    if key in data.keys():
+        return data[key]
+    else:
+        return 0
+
+def get_hash(filename):
+    hasher = hashlib.md5()
+    with open(filename,"rb") as i:
+        buf = i.read()
+        hasher.update(buf)
+        print("Hashed %s - %s" % (filename, hasher.hexdigest()))
+        
+        return hasher.hexdigest()
+    raise Exception("Hashing error")
+
+def get_subs(segment): # segment is the name of .ts file (without .ts)
+    r = sr.Recognizer()
+
+    infile = segment + ".ts"
+    outfile = segment + ".wav"
+
+
+
+    hs = get_hash(infile)
+    cache = get_cache(hs)
+    if cache != 0:
+        print("Found in cache")
+        return cache
+
+
+
+    subprocess.run(["ffmpeg", "-i", infile, outfile,"-y","-loglevel", "panic","-hide_banner","-nostats"])
+
+
+    segmentAudio = sr.AudioFile(segment + ".wav")
+    with segmentAudio as source:
+            audio = r.record(source)
+    sub = r.recognize_google(audio, language="ru_RU")
+    write_cache(hs,sub)
+    return sub
+
+
